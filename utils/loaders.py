35bb848d8e64be8ee7bf84fa598d1cff07d267fc
Anferdy <andresnevarez04@gmail.com>
2025-12-24 13:15:53 -0500

    conexiones con base de datos

---
+++ utils/loaders.py
@@ -0,0 +1,13 @@
+from flask import current_app
+import sqlite3
+from pathlib import Path
+
+def get_db_connection_flask():
+    db_path = current_app.config["DATABASE"]
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row  # permite dict(row)
+    return conn
+def get_db_connection_standalone():
+    db_path = Path(__file__).parent.parent / "db" / "inventario.db"
+    conn = sqlite3.connect(db_path)
+    return conn
